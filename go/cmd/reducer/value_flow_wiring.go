583891b2b7ca11f0a4d9f1a18c21af13d793fb8c
Allen Sanabria <asanabria6910@gmail.com>
2026-06-18 06:18:06 -0400

    Load correlated cloud sinks into value-flow fixpoint (#2968)
    * feat(reducer): load value-flow cloud sink targets
    
    * fix(reducer): load value-flow cloud action sinks

---
+++ go/cmd/reducer/value_flow_wiring.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log/slog"
+
+	"github.com/eshu-hq/eshu/go/internal/reducer"
+)
+
+func newValueFlowFixpointProjector(
+	summaryLoader reducer.FunctionSummarySnapshotLoader,
+	sourceLoader reducer.FunctionSourceSnapshotLoader,
+	graphIDLoader reducer.FunctionGraphIDSnapshotLoader,
+	graphReader reducer.GraphQueryRunner,
+	writer reducer.CodeInterprocEvidenceWriter,
+	logger *slog.Logger,
+) reducer.ValueFlowFixpointEvidenceProjector {
+	return reducer.ValueFlowFixpointEvidenceProjector{
+		Loader: reducer.ValueFlowFixpointEvidenceLoader{
+			SummarySnapshotLoader:   summaryLoader,
+			SourceSnapshotLoader:    sourceLoader,
+			GraphIDSnapshotLoader:   graphIDLoader,
+			CloudSinkSnapshotLoader: reducer.GraphValueFlowCloudSinkTargetLoader{Graph: graphReader},
+			Logger:                  logger,
+		},
+		Writer: writer,
+	}
+}
