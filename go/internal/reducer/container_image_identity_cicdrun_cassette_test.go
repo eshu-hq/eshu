5d68410e8a973f778d6a119e9d32994caf532343
Allen Sanabria <asanabria6910@gmail.com>
2026-08-09 01:50:56 -0400

    bind resource proof to staged source

--- go/internal/reducer/container_image_identity_cicdrun_cassette_test.go
+++ go/internal/reducer/container_image_identity_cicdrun_cassette_test.go
@@ -18,8 +18,8 @@ import (
 const (
 	lambdaImageDigest     = "sha256:0000000000000000000000000000000000000000000000000000000000cc"
 	lambdaImageRunID      = "5152"
-	lambdaImageRepository = "repository:r_69256c06"
-	lambdaImageScope      = "ci_cd_run:github_actions:eshu-hq:supply-chain-resource-proof"
+	lambdaImageRepository = "repository:r_217415d9"
+	lambdaImageScope      = "ci_cd_run:github_actions:acme:deployable-config-resource-proof"
 	lambdaImageGeneration = "cassette-cicd-scd-resource-gen1"
 )
 
