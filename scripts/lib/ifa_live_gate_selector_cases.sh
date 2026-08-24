79b680e181f8d83ed11ce6ddac16d3911ac345e6
Allen Sanabria <asanabria6910@gmail.com>
2026-08-24 00:03:07 -0400

    fix(ci): cover GCP relationship fault proof

--- scripts/lib/ifa_live_gate_selector_cases.sh
+++ scripts/lib/ifa_live_gate_selector_cases.sh
@@ -390,6 +390,7 @@ ifa_live_gate_fault_only_seams=(
 	'go/internal/projector/scope_generation_intents.go|go/internal/projector/scope_generation_intents.go'
 	'go/internal/projector/reducer_intent_fact_index.go|go/internal/projector/reducer_intent_fact_index.go'
 	'go/internal/projector/gcp/resource_materialization_intents.go|go/internal/projector/gcp/resource_materialization_intents.go'
+	'go/internal/projector/gcp/relationship_materialization_intents.go|go/internal/projector/gcp/relationship_materialization_intents.go'
 	'go/internal/reducer/defaults_additive_domains.go|go/internal/reducer/defaults_additive_domains.go'
 	'go/internal/reducer/defaults_additive_domains_cloud_nodes.go|go/internal/reducer/defaults_additive_domains_cloud_nodes.go'
 	'go/internal/reducer/defaults_additive_domains_gcp.go|go/internal/reducer/defaults_additive_domains_gcp.go'
